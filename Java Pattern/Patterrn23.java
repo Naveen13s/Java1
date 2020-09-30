c0e89532063fa975d522c1010555f4b44687dd38
Naveen kumar <55360964+Naveen13s@users.noreply.github.com>
2020-09-30 10:15:04 +0530

    Add files via upload

---
+++ Java Pattern/Patterrn23.java
@@ -0,0 +1,35 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class Patterrn23 {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		
+		int nst = 1;
+		int nsp = n - 1;
+		int val = 1;
+		//row
+		int row = 1;
+		while( row <= n) {
+			
+			//spaces
+			for(int csp = 1 ; csp <= nsp; csp++) {
+				System.out.print(" ");
+			}
+			//star
+			for(int cst = 1 ; cst <= nst; cst++) {
+				System.out.print(val);
+		    }
+			//prep
+			System.out.println();
+			nsp--;
+			nst +=2;
+			row++;
+			
+		}
+	}
+
+}
