e04b638b7bbfd9d286071e030377ac265682c062
Naveen13s <55360964+Naveen13s@users.noreply.github.com>
2020-09-30 09:10:46 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,13 @@
+
+public class Loop {
+
+	public static void main(String[] args) {
+		int counter = 1;
+		while (counter <= 10) {
+		     System.out.println("Hello Naveen");
+		    counter = counter + 1;
+	
+		}
+	}	
+
+}
