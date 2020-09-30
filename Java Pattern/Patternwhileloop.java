4d2d6d386147f1d16a05d24db6850320ac6d51f1
Naveen kumar <55360964+Naveen13s@users.noreply.github.com>
2020-09-30 10:16:36 +0530

    Add files via upload

---
+++ Java Pattern/Patternwhileloop.java
@@ -0,0 +1,27 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class Patternwhileloop {
+
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		
+		int row = 1;
+		while (row <= n) {
+			//work
+			int col = 1;
+			while (col <= n) {
+				System.out.print("*");
+				col = col + 1;
+			}
+			//preparation
+			System.out.println();
+			row = row + 1;
+		}
+
+	}
+
+}
